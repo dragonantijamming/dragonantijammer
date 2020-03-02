0ea2b2479827cc81694766d7eadc9d82defc6fec
ra648 <55992677+ra648@users.noreply.github.com>
2020-03-02 16:02:22 -0500

    fixed the port number

--- video_streaming/video_recv.sh
+++ video_streaming/video_recv.sh
@@ -3,5 +3,5 @@
 # Video streaming receiver
 sudo apt-get install vlc # Install VLC
 sed -i 's/geteuid/getppid/' /usr/bin/vlc # Allow vlc to run when root (just a bit of magic here)
-vlc udp://@:545454
+vlc udp://@:54545
 
